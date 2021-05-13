590fe5e0b38fe7f7935d153e7756bd312dea1007
4ngel5ire <4ngel5ire@gmail.com>
2021-05-13 08:51:26 -0500

    marquee working

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -6,20 +6,19 @@ function Sidebar() {
     <div className="sidebar">
       <div className="marquee">
         <p>
-          OPx Feed&nbsp;&nbsp; &nbsp;&nbsp; 🔥✉️🔥 &nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;Four Seasons Total Landscaping. Furries are
-          the Highest Form of Human Culture. Among all the chaotic happenings of
-          last week’s election, one particular incident might take the cake.
-          Though not actually “confirmed” (obviously due to embarrassment),
-          Trump held a post-election press conference last week at Four Seasons
-          Total Landscaping, a small and unspectacular local landscaping
-          business in Philly rather than the famous luxury hotel by the same
-          name, likely due to a booking mistake from his team. The
-          super-memeable happening has been immortalized in VR Chat as a new
-          spot for users to hang out in…which is nothing particularly notable
-          except for how it has quickly become a gathering spot for online VR
-          furries. Check out the desecration of Trump’s bootleg “Four Season’s”
-          press conference, here.
+          OPx Feed&nbsp;&nbsp; &nbsp; 🔥✉️🔥 &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Four
+          Seasons Total Landscaping. Furries are the Highest Form of Human
+          Culture. Among all the chaotic happenings of last week’s election, one
+          particular incident might take the cake. Though not actually
+          “confirmed” (obviously due to embarrassment), Trump held a
+          post-election press conference last week at Four Seasons Total
+          Landscaping, a small and unspectacular local landscaping business in
+          Philly rather than the famous luxury hotel by the same name, likely
+          due to a booking mistake from his team. The super-memeable happening
+          has been immortalized in VR Chat as a new spot for users to hang out
+          in…which is nothing particularly notable except for how it has quickly
+          become a gathering spot for online VR furries. Check out the
+          desecration of Trump’s bootleg “Four Season’s” press conference, here.
         </p>
       </div>
       <div className="content-sidebar">
