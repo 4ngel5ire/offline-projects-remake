e6bf006a8e25187d9de4bebb9d0d1abdfbcca3c7
4ngel5ire <4ngel5ire@gmail.com>
2021-05-16 15:32:53 -0500

    hero content and component finished w stylings. need to add seperate pages and server to fetch data

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -12,31 +12,38 @@ function Content() {
   }, []);
 
   function renderHeros() {
-    if (heroData?.heros) {
-      // return <Hero props={heroData.heros[0]} />
-    }
+    return (
+      <>
+        <Hero element={heroData?.heros[0]} />
+        <Hero element={heroData?.heros[1]} />
+        <Hero element={heroData?.heros[1]} />
+      </>
+    );
   }
 
   return (
     <div className="content">
       <div className="navbar">
-        <div className="navbar__title">Filters</div>
-        <a className="navbar__links">All</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links">Creative Direction</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links">Branding</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links">Experiential</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links">Live</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links">Film</a>
-        <div className="navbar__links__after">/</div>
-        <a className="navbar__links__final">Digital</a>
+        <div className="navbar__main">
+          <div className="navbar__title">Filters</div>
+          <a className="navbar__links">All</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links">Creative Direction</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links">Branding</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links">Experiential</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links">Live</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links">Film</a>
+          <div className="navbar__links__after">/</div>
+          <a className="navbar__links__final">Digital</a>
+        </div>
+        <div className="navbar__final">+</div>
       </div>
 
-      <div className="content__page">{renderHeros}</div>
+      <div className="content__page">{renderHeros()}</div>
     </div>
   );
 }
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -3,8 +3,38 @@ import "./Hero.css";
 import data from "./Data.json";
 import { HeroElement, Heros } from "./Data";
 
-function Hero(props: HeroElement) {
-  return <div>HERO</div>;
+interface HeroProps {
+  element: HeroElement | undefined;
+}
+
+function Hero(props: HeroProps) {
+  function renderImage() {
+    if (props.element?.img) {
+      return <img src={props.element.img} alt="" />;
+    }
+    return <></>;
+  }
+
+  return (
+    <div className="hero">
+      <div className="hero__text__ctn">
+        <div className="hero__text__title">{props.element?.artist}</div>
+        <div className="hero__text__subctn">
+          <div className="hero__text__subtitle">{props.element?.title}</div>
+          <div className="hero__text__p__ctn">
+            <div className="hero__text__p__title">
+              {props.element?.subHeading}
+            </div>
+            <div className="hero__text__p__content">
+              {props.element?.paragraph}
+            </div>
+          </div>
+        </div>
+      </div>
+      {renderImage()}
+      <div className="hero__img"></div>
+    </div>
+  );
 }
 
 export default Hero;
